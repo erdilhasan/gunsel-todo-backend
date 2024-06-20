4c65d3a3b6f3944e89539b87af990228cc85d313
erdilhasan <hasanerdil1260@gmail.com>
2024-06-20 12:03:50 +0300

    Add Todo and Create. User endpoints

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const http = require("http");
-var mongo = require("mongodb");
-
 const mongoose = require("mongoose");
-const { type } = require("os");
+var bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
 mongoose
   .connect("mongodb://127.0.0.1/gunsel_todo")
@@ -23,6 +22,7 @@ const TodoSchema = new mongoose.Schema({
   isComplete: Boolean,
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // OneToMany
 });
+
 const UserSchema = new mongoose.Schema({
   name: String,
   username: String,
@@ -30,13 +30,42 @@ const UserSchema = new mongoose.Schema({
   todos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Todo" }], // ManyToOne
 });
 
-const user = mongoose.model("User", UserSchema);
-const todo = mongoose.model("Todo", TodoSchema);
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+const User = mongoose.model("User", UserSchema);
+const Todo = mongoose.model("Todo", TodoSchema);
 
 app.get("/", (req, res) => {
   res.send("GÜNSEL To-Do App");
+});
+
+app.post("/api/user/create", async (req, res) => {
+  try {
+    const userData = req.body;
+    console.log(userData);
+    const user = User(userData);
+    user.save();
+    res.status(200).json({ message: "User Created Successfully." });
+  } catch (error) {
+    s;
+    console.log(error);
+    res.status(400).json({ message: "something went wrong." });
+  }
+});
+
+app.post("/api/todo/create", async (req, res) => {
+  // TODO: Implement Validation
+  try {
+    const todoData = req.body;
+    console.log(todoData);
+    const todo = Todo(todoData);
+    todo.save();
+    res.status(200).json({ message: "User Created Successfully." });
+  } catch (error) {
+    s;
+    console.log(error);
+    res.status(400).json({ message: "something went wrong." });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
